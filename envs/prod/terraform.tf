8e0272cc54bb6c3727fba0458952cbd599981ca5
shelvinx <shelvinx@outlook.com>
2025-10-03 16:08:48 +0100

    test: envs

---
+++ envs/prod/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = " ~> 4.46.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = " ~> 3.7.2"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-management"
+    storage_account_name = "tfauratest"
+    container_name       = "tfstate"
+    key                  = "prod.tfstate"
+    use_azuread_auth     = true
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
---
+++ envs/prod/variables.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group Name"
+}
+
+variable "location" {
+  type        = string
+  description = "Location"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment Name"
+  default     = "prod"
+}
+
+variable "account_tier" {
+    type = string
+    description = "Account Tier"
+}
+
+variable "account_replication_type" {
+    type = string
+    description = "Account Replication Type"
+}
+
+variable "app_service_plan_sku" {
+    type = string
+    description = "App Service Plan SKU"
+}
